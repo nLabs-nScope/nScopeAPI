d9309d833bc44a4a71debd64f4d2e51469e638b4
David Meyer <meyerdavidj@gmail.com>
2016-05-16 21:27:21 -0500

    added to sound visualizer example

--- python/examples/soundVisualizer.py
+++ python/examples/soundVisualizer.py
@@ -9,19 +9,33 @@ nScope = nScopeObj()
 sampleRate = 44100.0
 numSamples = 1152
 
-data = ones(numSamples)
+fig = figure(figsize=(8,8))
+timeAx = subplot(2,1,1)
+data = nScope.readCh1(numSamples,sampleRate)
 time = arange(numSamples)/sampleRate*1000.0
-freq = rfftfreq(numSamples)*sampleRate
-
 axis([0,max(time),-2,2])
 xlabel('time (ms)')
 ylabel('voltage (v)')
 
 voltage, = plot(time,data)
 
+freqAx = subplot(2,1,2)
+freq = rfftfreq(numSamples)*sampleRate
+fftdata = abs(rfft(data))
+xlabel('frequency (Hz)')
+ylabel('magnitude')
+spectrum, = plot(freq,fftdata)
+axis([20,20000,0,numSamples])
+freqAx.set_xscale('log')
+freqAx.set_yscale('log')
+
+
 def update(event):
     data = nScope.readCh1(numSamples,sampleRate)
+    fftdata = abs(rfft(data))
     voltage.set_ydata(data)
+    spectrum.set_ydata(fftdata)
+
 
 ani = animation.FuncAnimation(fig, update, interval=16)
 
