f74acd3fad70a5592555d333cb9986d282c8af06
Martino Ferrari <manda.mgf@gmail.com>
2021-01-30 23:06:55 +0100

    script for monitoring power and updated red-node

--- python/testConnection.py
+++
@@ -1,8 +0,0 @@
-try:
-    import nscopeapi as nsapi
-    ns = nsapi.nScope()
-except Exception as e:
-    print("Unable to communicate with nScope")
-    print(e)
-else:
-    print("Successfully opened connection to nScope!")
